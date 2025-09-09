1334e17359d30ccffe7f1600b30a634dabfe3b78
MagmaSKV <82288997+MagmaSKV@users.noreply.github.com>
2025-09-09 03:53:40 +0200

    Update hotkey_service.cpp

--- src/services/hotkey/hotkey_service.cpp
+++ src/services/hotkey/hotkey_service.cpp
@@ -37,6 +37,7 @@ namespace big
 		register_hotkey("vehiclefly", g.settings.hotkeys.vehicle_flymode, "vehiclefly"_J);
 		register_hotkey("waypoint", g.settings.hotkeys.teleport_waypoint, "waypointtp"_J);
 		register_hotkey("highlighttp", g.settings.hotkeys.teleport_selected, "highlighttp"_J);
+		register_hotkey("bringallplayers", g.settings.hotkeys.beastjump, "bringallplayers"_J);
 
 		g_renderer.add_wndproc_callback([this](HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
 			wndproc(static_cast<eKeyState>(msg), wparam);
